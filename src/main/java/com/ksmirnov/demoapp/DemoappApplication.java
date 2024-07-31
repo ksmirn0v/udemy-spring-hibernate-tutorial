46e1f9065c69a67eae0245a46952d90f33de9c83
ksmirnov <kirill.smirnov@konux.de>
2024-08-01 00:45:54 +0200

    Section 2-73: Saving a Java Object with JPA - Coding - Part 2

--- src/main/java/com/ksmirnov/demoapp/DemoappApplication.java
+++ src/main/java/com/ksmirnov/demoapp/DemoappApplication.java
@@ -1,5 +1,7 @@
 package com.ksmirnov.demoapp;
 
+import com.ksmirnov.demoapp.dao.StudentDAO;
+import com.ksmirnov.demoapp.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,20 @@ public class DemoappApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World!");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		System.out.println("Creating a new student object");
+		Student student = new Student("Kirill", "Smirnov", "ksmirnov@mail.com");
+
+		System.out.println("Saving the new student object");
+		studentDAO.save(student);
+
+		System.out.println("The id of the saved student = " + student.getId());
 	}
 }
---
+++ src/main/java/com/ksmirnov/demoapp/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.ksmirnov.demoapp.dao;
+
+import com.ksmirnov.demoapp.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+}
