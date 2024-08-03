e2b95f3ab9533b80e149b818cbbc7e82a2e3b9b4
ksmirnov <kirill.smirnov@konux.de>
2024-08-03 15:02:19 +0200

    Section 3-96: Spring Boot Rest Controller - Coding

--- src/main/java/com/ksmirnov/demoapp/DemoappApplication.java
+++ src/main/java/com/ksmirnov/demoapp/DemoappApplication.java
@@ -1,34 +1,12 @@
 package com.ksmirnov.demoapp;
 
-import com.ksmirnov.demoapp.dao.StudentDAO;
-import com.ksmirnov.demoapp.entity.Student;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoappApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
-		return runner -> {
-			readStudents(studentDAO);
-		};
-	}
-
-	private void readStudents(StudentDAO studentDAO) {
-
-		System.out.println("Creating new student objects");
-		Student student1 = new Student("John", "Doe", "jdoe@mail.com");
-		Student student2 = new Student("Michael", "Bobick", "mbobick@mail.com");
-
-		System.out.println("Saving the new student objects");
-		studentDAO.save(student1);
-		studentDAO.save(student2);
 	}
 }
