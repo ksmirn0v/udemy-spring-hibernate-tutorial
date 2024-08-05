e2d5718db3d63b82d48947a1e927c1d1329ee684
ksmirnov <kirill.smirnov@konux.de>
2024-08-05 14:19:22 +0200

    Section 3-103: Spring Boot REST Path Variables - Coding - Part 2

--- src/main/java/com/ksmirnov/demoapp/rest/MainController.java
+++ src/main/java/com/ksmirnov/demoapp/rest/MainController.java
@@ -1,7 +1,9 @@
 package com.ksmirnov.demoapp.rest;
 
 import com.ksmirnov.demoapp.entities.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,23 @@ import java.util.List;
 @RequestMapping("/api")
 public class MainController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> students;
 
-        List<Student> students = new ArrayList<>();
-
+    @PostConstruct
+    public void loadStudents() {
+        students = new ArrayList<>();
         students.add(new Student("Denzel", "Waschington"));
         students.add(new Student("Leonardo", "DiCaprio"));
         students.add(new Student("Tom", "Hanks"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable("studentId") int id) {
+        return students.get(id);
     }
 }
